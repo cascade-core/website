cd1e7ab57f242b96e8cd43f0a8f33f5f78671921
Josef Kufner <jk@frozen-doe.net>
2013-04-12 13:09:01 +0200

    Google Analytics code

--- app/block/page/skeleton.ini.php
+++ app/block/page/skeleton.ini.php
@@ -60,7 +60,17 @@ enable[] = "skeleton:done"
 data = "<!-- html head -->
 <link rel='stylesheet' href='/core/style/basic.css' type='text/css'>
 <link rel='stylesheet' href='/app/style/main.css' type='text/css'>
-"
+
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-22907817-2']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>"
 slot = "html_head"
 slot_weight = "20"
 
