68bec90447b59cf281a3d27cc1dffca3fde5df19
Josef Kufner <jk@frozen-doe.net>
2013-05-31 15:08:50 +0200

    Show few entities from SmallDB

--- app/entities.json.php
+++ app/entities.json.php
@@ -48,6 +48,97 @@
 					}
 				}
 			}
+		},
+		"article": {
+			"state_machine": {
+				"class": "\\SmallDb\\StateMachine\\ArrayMachine",
+				"states": {
+					"writing": {
+						"description": ""
+					},
+					"submitted": {
+						"description": ""
+					},
+					"waiting": {
+						"description": ""
+					},
+					"published": {
+						"description": ""
+					},
+					"rejected": {
+						"description": ""
+					}
+				},
+				"actions": {
+					"create": {
+						"returns": "new_id",
+						"transitions": {
+							"": {
+								"targets": [ "writing" ]
+							}
+						}
+					},
+					"edit": {
+						"transitions": {
+							"writing": {
+								"targets": [ "writing" ]
+							},
+							"submitted": {
+								"targets": [ "submitted" ]
+							}
+						}
+					},
+					"submit": {
+						"transitions": {
+							"writing": {
+								"targets": [ "submitted" ]
+							}
+						}
+					},
+					"withdraw": {
+						"transitions": {
+							"submitted": {
+								"targets": [ "writing" ]
+							}
+						}
+					},
+					"return": {
+						"transitions": {
+							"submitted": {
+								"targets": [ "writing" ]
+							}
+						}
+					},
+					"accept": {
+						"transitions": {
+							"submitted": {
+								"targets": [ "waiting", "published" ]
+							}
+						}
+					},
+					"publish": {
+						"transitions": {
+							"waiting": {
+								"targets": [ "published" ]
+							}
+						}
+					},
+					"hide": {
+						"transitions": {
+							"published": {
+								"targets": [ "submitted" ]
+							}
+						}
+					},
+					"reject": {
+						"transitions": {
+							"submitted": {
+								"targets": [ "rejected" ]
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 }
