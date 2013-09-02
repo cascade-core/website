6c9a9f5a53f354163044339d451639e646209638
Josef Kufner <jk@frozen-doe.net>
2013-09-02 22:25:19 +0200

    SmallDB machine visualization

--- app/entities.json.php
+++ app/entities.json.php
@@ -73,6 +73,215 @@
 				}
 			}
 		},
+		"auction_item": {
+			"state_machine": {
+				"class": "\\SmallDb\\StateMachine\\ArrayMachine",
+				"states": {
+					"preparing": {
+						"label": "Příprava aukce"
+					},
+					"finished": {
+						"label": "Aukce ukončena"
+					},
+					"limiting": {
+						"label": "Limitování",
+						"group": "pre_auction"
+					},
+					"pre_auction": {
+						"label": "Před-aukce",
+						"group": "pre_auction"
+					},
+					"hall_auction": {
+						"label": "Sálová aukce",
+						"group": "auction"
+					},
+					"online_auction_waiting": {
+						"label": "Čeká v pořadí",
+						"group": "online_auction"
+					},
+					"online_auction_active": {
+						"label": "Aktivní",
+						"group": "online_auction"
+					},
+					"online_auction_done": {
+						"label": "Ukončeno",
+						"group": "online_auction"
+					},
+					"e_auction_active": {
+						"label": "Aktivní",
+						"group": "e_auction"
+					},
+					"e_auction_done": {
+						"label": "Ukončeno",
+						"group": "e_auction"
+					},
+					"store_active": {
+						"label": "Aktivní",
+						"group": "store"
+					},
+					"store_sold": {
+						"label": "Prodáno",
+						"group": "store"
+					}
+				},
+				"state_groups": {
+					"auction": {
+						"color": "transparent",
+						"groups": {
+							"pre_auction": {
+								"color": "transparent"
+							},
+							"online_auction": {
+								"label": "Online aukce",
+								"color": "#66cc22"
+							}
+						}
+					},
+					"misc": {
+						"color": "transparent",
+						"groups": {
+							"e_auction": {
+								"label": "e-aukce",
+								"color": "#6666cc"
+							},
+							"store": {
+								"label": "Obchod",
+								"color": "#aa6622"
+							}
+						}
+					}
+				},
+				"actions": {
+					"create": {
+						"transitions": {
+							"": {
+								"targets": [ "preparing" ]
+							}
+						}
+					},
+					"edit": {
+						"transitions": {
+							"preparing": {
+								"targets": [ "preparing" ]
+							}
+						}
+					},
+					"publish": {
+						"transitions": {
+							"preparing": {
+								"targets": [ 
+									"limiting", "pre_auction",
+									"hall_auction", "online_auction_waiting", "e_auction_active",
+									"store_active"
+								]
+							}
+						}
+					},
+					"next": {
+						"transitions": {
+							"limiting": {
+								"targets": [ "hall_auction", "online_auction_waiting" ]
+							},
+							"pre_auction": {
+								"targets": [ "hall_auction", "online_auction_waiting" ]
+							},
+							"hall_auction": {
+								"targets": [ "finished" ]
+							},
+							"online_auction_done": {
+								"targets": [ "finished" ]
+							},
+							"e_auction_done": {
+								"targets": [ "finished" ]
+							},
+							"store_active": {
+								"targets": [ "finished" ]
+							},
+							"store_sold": {
+								"targets": [ "finished" ]
+							}
+						}
+					},
+					"limit": {
+						"transitions": {
+							"limiting": {
+								"targets": [ "limiting" ]
+							}
+						}
+					},
+					"pre_bid": {
+						"transitions": {
+							"pre_auction": {
+								"targets": [ "pre_auction" ]
+							}
+						}
+					},
+					"hall_set_results": {
+						"transitions": {
+							"hall_auction": {
+								"targets": [ "hall_auction" ]
+							}
+						}
+					},
+					"e_bid": {
+						"transitions": {
+							"e_auction_active": {
+								"targets": [ "e_auction_active" ]
+							}
+						}
+					},
+					"e_stop": {
+						"transitions": {
+							"e_auction_active": {
+								"targets": [ "e_auction_done" ]
+							}
+						}
+					},
+					"online_start": {
+						"transitions": {
+							"online_auction_waiting": {
+								"targets": [ "online_auction_active" ]
+							}
+						}
+					},
+					"online_bid": {
+						"transitions": {
+							"online_auction_active": {
+								"targets": [ "online_auction_active" ]
+							}
+						}
+					},
+					"online_going": {
+						"transitions": {
+							"online_auction_active": {
+								"targets": [ "online_auction_active" ]
+							}
+						}
+					},
+					"online_stop": {
+						"transitions": {
+							"online_auction_active": {
+								"targets": [ "online_auction_done" ]
+							}
+						}
+					},
+					"online_back": {
+						"transitions": {
+							"online_auction_done": {
+								"targets": [ "online_auction_active" ]
+							}
+						}
+					},
+					"store_sell": {
+						"transitions": {
+							"store_active": {
+								"targets": [ "store_sold" ]
+							}
+						}
+					}
+				}
+			}
+		},
 		"article": {
 			"state_machine": {
 				"class": "\\SmallDb\\StateMachine\\ArrayMachine",
