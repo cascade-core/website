639c79ce18db9237fb29d3197271eb7aa642a637
Josef Kufner <jk@frozen-doe.net>
2014-01-19 21:06:35 +0100

    Add links to doxygen into documentation

--- app/admin.json.php
+++ app/admin.json.php
@@ -1,10 +1,26 @@
 {
 	"_": "<?php printf('_%c%c}%c',34,10,10);__halt_compiler();?>",
 	"main_menu": {
+		"documentation": {
+			"children": {
+				"doxygen": {
+					"title": "Doxygen docs",
+					"link": "/admin/doc/doxygen"
+				}
+			}
+		},
 		"exit": {
 			"title": "Exit administration",
 			"link": "/",
 			"weight": 90
+		}
+	},
+	"routes": {
+		"/doc/doxygen": {
+			"title": "Doxygen documentation",
+			"block": "page/doxygen_links",
+			"connections": {
+			}
 		}
 	}
 }
---
+++ app/block/page/doxygen_links.json.php
@@ -0,0 +1,21 @@
+{
+    "_": "<?php printf('_%c%c}%c',34,10,10);__halt_compiler();?>",
+    "outputs": {
+        "done": true
+    },
+    "block:raw": {
+        ".block": "core/out/raw",
+        ".x": 170,
+        ".y": 32,
+        "data": "<ul>\n  <li><a target=\"_blank\" href=\"/core/doc/doxygen/html/index.html\">Cascade Core</a></li>\n  <li><a target=\"_blank\" href=\"/plugin/smalldb/doc/doxygen/html/index.html\">Smalldb</a></li>\n</ul>",
+        "slot_weight": 40
+    },
+    "block:header": {
+        ".block": "core/out/header",
+        ".x": 0,
+        ".y": 0,
+        "level": 2,
+        "text": "Doxygen generated documentation",
+        "slot_weight": 10
+    }
+}
