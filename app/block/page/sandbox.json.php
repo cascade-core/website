3ce1d393655cac2272756f7cd784eed2f68f61ca
Josef Kufner <jk@frozen-doe.net>
2014-04-27 10:51:43 +0200

    Nicer and image-less message boxes
    Messages use same color as blocks in diagrams

---
+++ app/block/page/sandbox.json.php
@@ -0,0 +1,18 @@
+{
+    "_": "<?php printf('_%c%c}%c',34,10,10);__halt_compiler();?>",
+    "routes": {
+        "/": {
+            "title": "Hello"
+        }
+    },
+    "blocks": {
+        "raw": {
+            "block": "core/out/raw",
+            "x": 0,
+            "y": 0,
+            "in_val": {
+                "data": "<h2>Sandbox</h2>\n\n<div class=\"message message_error\">\n<h2>Sorry.</h2>\n<p>Lorem ipsum dolor sit amet.</p>\n</div>\n\n<div class=\"message message_success\">\n<h2>Good.</h2>\n<p>Lorem ipsum dolor sit amet.</p>\n</div>\n\n<div class=\"message message_info\">\n<h2>Information.</h2>\n<p>Lorem ipsum dolor sit amet.</p>\n</div>\n"
+            }
+        }
+    }
+}
