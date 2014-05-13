971dfd6d7e4f45eaf38bac3ed52953ff72251a1d
Josef Kufner <jk@frozen-doe.net>
2014-05-14 00:11:46 +0200

    Link to papers

--- app/block/page/hello.json.php
+++ app/block/page/hello.json.php
@@ -11,7 +11,7 @@
             "x": 0,
             "y": 0,
             "in_val": {
-                "data": "<h2>Hello!</h2>\n\n<p>One day this will be a real presentation of cascade-based framework. For now, it is just an experimental sandbox, but feel free to take a look.</p>\n\n<p>If you have any questions or comments, please send me an e-mail to\n<a href=\"mailto:jk&#64;frozen-doe&#46;net?subject=[cascade]%20...\">\njk&#64;frozen-doe&#46;net</a>.</p>"
+                "data": "<h2>Hello!</h2>\n\n<p>One day this will be a real presentation of cascade-based framework. For now, it is just an experimental sandbox, but feel free to take a look.</p>\n\n<p>You may find some theoretical background of this demo in papers at <a href=\"http://jk.frozen-doe.net/publications/\">http://jk.frozen-doe.net/publications/</a>.</p>\n\n<p>If you have any questions or comments, please send me an e-mail to\n<a href=\"mailto:jk&#64;frozen-doe&#46;net?subject=[cascade]%20...\">\njk&#64;frozen-doe&#46;net</a>.</p>"
             }
         }
     }
