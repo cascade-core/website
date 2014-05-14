92d4d9feb31c115a975e773339800418dad4084b
Josef Kufner <jk@frozen-doe.net>
2014-05-14 12:43:45 +0200

    Nice introduction page, drop duplicate profiler page

--- app/main_menu.json.php
+++ app/main_menu.json.php
@@ -1,12 +1,8 @@
 {
     "_": "<?php printf('_%c%c}%c',34,10,10);__halt_compiler();?>",
     "hello": {
-        "title": "Hello",
+        "title": "Introduction",
         "link": "/"
-    },
-    "profiler": {
-        "title": "Profiler",
-        "link": "/profiler"
     },
     "admin": {
         "title": "Administration",
