84b022d98611f94870bf4c0cff16c9d9fdf980c6
Josef Kufner <jk@frozen-doe.net>
2014-10-02 12:14:44 +0200

    Convert submodules to pure Composer packages

--- app/admin.json.php
+++ app/admin.json.php
@@ -1,23 +1,4 @@
 {
     "_": "<?php printf('_%c%c}%c',34,10,10);__halt_compiler();?>",
-    "main_menu": {
-        "documentation": {
-            "children": {
-                "doxygen": {
-                    "title": "Doxygen docs",
-                    "link": "/admin/doc/doxygen"
-                }
-            }
-	}
-    },
-    "common_block": "admin_extras",
-    "routes": {
-        "/doc/doxygen": {
-            "title": "Doxygen documentation",
-            "block": "page/doxygen_links",
-            "connections": [
-
-            ]
-        }
-    }
+    "common_block": "admin_extras"
 }
